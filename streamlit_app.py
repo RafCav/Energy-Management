b8af106c18b7851efc94b6b9d73e9d3019575f47
Rafael Cavallari <76668402+RafCav@users.noreply.github.com>
2024-08-19 01:02:42 -0300

    Update streamlit_app.py
    Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,61 @@
 import streamlit as st
+from datetime import datetime
+import pandas as pd
+import pytz
+import matplotlib.pyplot as plt
 
 
-def screen_home():
-    st.title('Controle de Energia')
+def get_data():
+    sheet_id = '14ZRdMa16p6LR9BKdDGXYXYGYOqe5auFOKEUS1J4U40Y'
+    gid = '640773164'
 
-    # Creating two buttons side by side
-    col1, col2 = st.columns(2)
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}")
 
-    with col1:
-        button_register = st.button('Cadastro')
+    df['date'] = pd.to_datetime(df['date'])
+    df['value_per_kwh'] = df['value_per_kwh'].str.replace(',', '.').astype(float)
+    df['value'] = df['value'].str.replace(',', '.').astype(float)
 
-    with col2:
-        button_projection = st.button('Projeção')
-
-    # Send to the page after clicking the button
-    if button_register:
-        st.session_state['screen'] = 'register'
-        st.rerun()
-
-    if button_projection:
-        st.session_state['screen'] = 'projection'
-        st.rerun()
+    return df
 
 
-def screen_register():
-    st.title('Tela de Cadastro')
-    st.write('Você está na tela de cadastro.')
+st.title('Projeção da Conta de Energia')
 
-    # Back to the home
-    button_home = st.button('Voltar')
-    if button_home:
-        st.session_state['screen'] = 'home'
-        st.rerun()
+with st.form(key='projection_form'):
+    kwh_current = st.number_input('Digite o kWh consumido:', format='%d', step=1)
+    submit_button = st.form_submit_button(label='Enviar')
 
+    if submit_button:
+        df = get_data()
 
-def screen_projection():
-    st.title('Tela de Projeção')
-    st.write('Você está na tela de projeção.')
+        # Date Variables
+        today = datetime.now(pytz.timezone('America/Sao_Paulo')).date()  # Just today :p
+        date_next_billing = (df['date'].max()).date()  # Next billing date
+        date_last_billing = df['date'].iloc[len(df) - 2].date()  # Last billing date
+        days_next_billing = (date_next_billing - date_last_billing).days  # Days between today and next billing date
+        days_last_billing = (today - date_last_billing).days  # Days between last billing date and today
 
-    # Back to the home
-    button_home = st.button('Voltar')
-    if button_home:
-        st.session_state['screen'] = 'home'
-        st.rerun()
+        # Math Variables
+        avg_value_per_kwh = df['value_per_kwh'].mean()  # Average billing cost per kWh
+        last_kwh = df['kWh'].max()  # Last kWh registered
+        kwh_consumed = int(kwh_current) - last_kwh  # Difference between last and current kWh
 
+        # Projection Variables
+        kwh_projection = (days_next_billing * kwh_consumed) / days_last_billing
+        value_projection = kwh_projection * avg_value_per_kwh
 
-# Inicializando a tela padrão na sessão
-if 'screen' not in st.session_state:
-    st.session_state['screen'] = 'home'
+        # Fill the black space with the projection
+        df['value'] = df['value'].fillna(value_projection)
 
-# Exibindo a tela correta com base no estado da sessão
-if st.session_state['screen'] == 'home':
-    screen_home()
-elif st.session_state['screen'] == 'register':
-    screen_register()
-elif st.session_state['screen'] == 'projection':
-    screen_projection()
+        st.write(f'Valor da Conta Projetado: R$ {round(value_projection, 2)}')
+        st.write(f'Consumo Projetado: {kwh_projection} kWh')
+
+        # DataFrame cut
+        df_copied = df.tail(6)
+
+        # Plot
+        fig, ax = plt.subplots()
+        ax.bar(df_copied['date'], df_copied['value'], width=15)
+        ax.set_xlabel('Mês')
+        ax.set_ylabel('R$')
+        ax.set_title('Valor das Contas por Data')
+        st.pyplot(fig)
